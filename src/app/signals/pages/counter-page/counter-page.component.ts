8d85fa0d40321b6096ecc30fa395289c1d925403
gaburra-m <gaburra.m@gmail.com>
2024-02-07 17:40:04 -0600

    Signals, Computed y Effect

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
-  styleUrls: ['./counter-page.component.css']
+  styleUrls: ['./counter-page.component.css'],
 })
 export class CounterPageComponent {
+  public counter = signal(10);
+  public squareCounter = computed(() => this.counter() * this.counter()); //<- los computed son de solo lectura.
 
+  increaseBy(value: number) {
+    // this.counter.set(this.counter() + value); <- una forma de hacerlo, pero no se ve bien.
+
+    this.counter.update((current) => current + value);
+  }
 }
--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,9 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
-  styleUrls: ['./properties-page.component.css']
+  styleUrls: ['./properties-page.component.css'],
 })
 export class PropertiesPageComponent {
+  public counter = signal(10);
+  public user = signal<User>({
+    id: 1,
+    email: 'gabo@gmail.com',
+    first_name: 'Gabriel',
+    last_name: 'Morales',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  });
 
+  public fullName = computed(
+    () => `${this.user().first_name} ${this.user().last_name}`
+  );
+
+  public userChangedEffect = effect(() => {
+    // los efectos se autodestruyen.
+    console.log(`Effect: ${this.user().first_name} - ${this.counter()}`);
+  });
+
+  increaseBy(value: number) {
+    this.counter.update((current) => current + value);
+  }
+
+  onFieldUpdated(field: keyof User, value: string) {
+    // this.user.set({ <- Es una manera de hacerlo pero algo peligrsa
+    //   ...this.user(),
+    //   [field]: value,
+    // });
+    //
+    // La misma manera de hacerlo pero con update, algo peligrosa
+    // this.user.update(current => ({
+    //   ...current,
+    //   [field]: value
+    // }))
+
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+
+        case 'avatar':
+          current.avatar = value;
+          break;
+
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+
+      return current;
+    });
+  }
 }
